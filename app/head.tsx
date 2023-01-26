9a487cb0018cb843a2055ef4819ba7779ec8a913
Thomas Cazade <cazade.thomas@gmail.com>
2023-01-26 15:05:08 +0100

    feat: add meta-og image

--- app/head.tsx
+++ app/head.tsx
@@ -2,11 +2,14 @@ export default function Head() {
   const title = "McDonald's France Price Comparator";
   const description =
     "Compare the price of McDonald's burgers in every French restaurant and find the cheapest store near you.";
+  const image =
+    "https://mcdonald-price-comparator.vercel.app/_static/meta-og.jpg";
 
   return (
     <>
       <title>McDonald&apos;s France Price Comparator</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta property="image" content={image} />
       <meta
         name="description"
         content="Compare the price of McDonald's burgers in every French restaurant and find the cheapest store near you."
@@ -16,7 +19,9 @@ export default function Head() {
       <meta property="og:site_name" content={title} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
       <meta property="og:url" content="https://fastsheet.co/" />
+      <meta property="og:image:alt" content={description} />
       <meta property="og:image:alt" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="en_US" />
@@ -26,6 +31,7 @@ export default function Head() {
       <meta name="twitter:creator" content="@totominc" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
     </>
   );
 }
